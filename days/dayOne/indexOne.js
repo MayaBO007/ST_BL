530b10b3c6cfa18f6634d12fc8b5c17b58728ffe
Maya <mayabaror@gmail.com>
2023-01-10 15:17:38 +0200

    index fix

--- days/dayOne/indexOne.js
+++ days/dayOne/indexOne.js
@@ -2,49 +2,32 @@
 
 // move to main function
 function timeline() {
-    let startDayOne = async function () {
-        platform.getAllSessions().then((data) => {
-            getIndexSessionData(data).then((i) => {
-                studySessionData = data[i];
-                let updatedDates = updateDates();
-                if (updatedDates.fullDate.getDate() == updatedDates.yesterday.getDate()) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {
-                    document.getElementById("fiveAM").style.display = "inline";
-                    setTimeout(() => {
-                        moveToDay();
-                    }, timeToFive());
-                }
-                else if (updatedDates.fullDate.getDate() == updatedDates.yesterdayPlusOne.getDate()) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {)
-                    if (0 <= updatedDates.fullDate.getHours() & updatedDates.fullDate.getHours() < 5) {
-                        document.getElementById("fiveAM").style.display = "inline";
+    platform.getAllSessions().then((data) => {
+        console.log(data);
+        studySessionData = data[0];
+        let updatedDates = updateDates();
+        if (updatedDates.fullDate.getDate() == Number(dayDate())) {
+            deleteFromSessionData();
+            let goOne = async function () {
+                let doneDay1 = await start2tests(); // add promise and resolve
+                if (doneDay1 == "doneDayOne") {
+                    studySessionData.doneDay1 = "doneDayOne";
+                    studySessionData.expDaysDate = updatedDates.fullDate;
+                    platform.saveSession(studySessionData, true)
+                    document.getElementById("endDayMsg").style.display = "inline";
+                    document.getElementById("endDayMsg").addEventListener("click", function () {
+                        showWinnings()
                         setTimeout(() => {
-                            moveToDay();
-                        }, timeToFiveSameDay());
-                    } else {
-                        let goOne = async function () {
-                            deleteFromSessionData();
-                            let doneDayOne = await start2tests(); // add promise and resolve
-                            if (doneDayOne == "doneDayOne") {
-                                studySessionData.doneDay1 = "doneDayOne";
-                                studySessionData.expDaysDate = updatedDates.fullDate;
-                                platform.saveSession(studySessionData, true)
-                                document.getElementById("endDayMsg").style.display = "inline";
-                                document.getElementById("endDayMsg").addEventListener("click", function () {
-                                    showWinnings()
-                                    setTimeout(() => {
-                                        platform.goToUrl("days/dayTwo/dayTwo.html");
-                                    }, 7000)
-                                })
-                            }
-                        }
-                        goOne()
-                    }
-                } else {
-                    document.getElementById("endOfGame").style.display = "inline";
+                            platform.goToUrl("days/dayTwo/dayTwo.html");
+                        }, 7000)
+                    })
                 }
-            })
-        });
-    }
-    startDayOne()
+            }
+            goOne();
+        } else {
+            document.getElementById("endOfGame").style.display = "inline";
+        }
+    })
 }
 
 
